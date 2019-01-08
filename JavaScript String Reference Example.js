f1524ace7e1ebf2a3c9e7976e53acfc3cc2f53b1
Busra <46317863+BusraOzdmr@users.noreply.github.com>
2019-01-08 22:38:02 +0200

    Update JavaScript String Reference Example.js

--- JavaScript String Reference Example.js
+++ JavaScript String Reference Example.js
@@ -17,19 +17,19 @@
 	var c = "Boolean=3";
     
   	var ailk = a.charAt(0);
-    var ason = a.charAt(a.length-1);
+    	var ason = a.charAt(a.length-1);
 
 	var bilk = b.charAt(0);
-    var bson = b.charAt(b.length-1);
+    	var bson = b.charAt(b.length-1);
 
-    var cilk = c.charAt(0);
-    var cson = c.charAt(c.length-1);
+    	var cilk = c.charAt(0);
+    	var cson = c.charAt(c.length-1);
     
 function Fonksiyon1() {
     
-console.log("a stringine ait ilk karakter " + ailk + " son karakter " + ason + 
-   "\nb stringine ait ilk karakter " + bilk + " son karakter " + bson + 
-   "\nc stringine ait ilk karakter " + cilk + " son karakter " + cson );
+	console.log("a stringine ait ilk karakter " + ailk + " son karakter " + ason + 
+   		"\nb stringine ait ilk karakter " + bilk + " son karakter " + bson + 
+   		"\nc stringine ait ilk karakter " + cilk + " son karakter " + cson );
 }
 
 function Fonksiyon2() {
@@ -39,45 +39,45 @@ function Fonksiyon2() {
 
 function Fonksiyon3() {
 	
-    console.log("a stringine ait son karakter " + a.charAt(a.length-1) +
-    	"\nb stringine ait son karakter " + b.charAt(b.length-1) +
-        "\nc stringine ait son karakter " + c.charAt(c.length-1));
+    	console.log("a stringine ait son karakter " + a.charAt(a.length-1) +
+    		"\nb stringine ait son karakter " + b.charAt(b.length-1) +
+       		 "\nc stringine ait son karakter " + c.charAt(c.length-1));
 }
 
 function Fonksiyon4() {
 
 	var x = a.slice(-1);
-    var y = b.slice(-1);
-    var z = c.slice(-1);
+ 	var y = b.slice(-1);
+	var z = c.slice(-1);
     
-    console.log(x.concat(y,z));
+    	console.log(x.concat(y,z));
 }
 
 function Fonksiyon5() {
     
         console.log("a stringine ait sondan bir önceki karakter " + a.charAt(a.length-2) + 
-    	"\nb stringine ait sondan bir önceki karakter " + b.charAt(b.length-2) +
-        "\nc stringine ait sondan bir önceki karakter " + c.charAt(c.length-2));    
+    		"\nb stringine ait sondan bir önceki karakter " + b.charAt(b.length-2) +
+        	"\nc stringine ait sondan bir önceki karakter " + c.charAt(c.length-2));    
 }
 
 function Fonksiyon6() {
     
         console.log("a stringinin uzunluğu " + a.length + 
-    	"\nb stringinin uzunluğu " + b.length +
-        "\nc stringinin uzunluğu " + c.length );
+    		"\nb stringinin uzunluğu " + b.length +
+        	"\nc stringinin uzunluğu " + c.length );
 }
 
 function Fonksiyon7() {
 
-		console.log("a stringi içinde a karakterinin indexi " + a.search("a") +
+	console.log("a stringi içinde a karakterinin indexi " + a.search("a") +
         	"\nb stringi içinde a karakterinin indexi " + b.search("a") +
-            "\nc stringi içinde a karakterinin indexi " + c.search("a") +
-            "\n\na stringi içinde . karakterinin indexi " + a.search("\\.") +
-            "\nb stringi içinde . karakterinin indexi " + b.search("\\.") +
-            "\na stringi içinde . karakterinin indexi " + c.search("\\.") +
-            "\n\na stringi içinde 1 yada 2 karakterinin indexi " + a.search("1|2") +
-            "\nb stringi içinde 1 yada 2 karakterinin indexi " + b.search("1|2") +
-            "\nc stringi içinde 1 yada 2 karakterinin indexi " + c.search("1|2"));
+            	"\nc stringi içinde a karakterinin indexi " + c.search("a") +
+            	"\n\na stringi içinde . karakterinin indexi " + a.search("\\.") +
+            	"\nb stringi içinde . karakterinin indexi " + b.search("\\.") +
+            	"\na stringi içinde . karakterinin indexi " + c.search("\\.") +
+            	"\n\na stringi içinde 1 yada 2 karakterinin indexi " + a.search("1|2") +
+            	"\nb stringi içinde 1 yada 2 karakterinin indexi " + b.search("1|2") +
+            	"\nc stringi içinde 1 yada 2 karakterinin indexi " + c.search("1|2"));
             
 }
 </script>
